a364b3ec1d0219e8c0c64f7ef81032d2d8608b23
Hex621 <reppeti@gmail.com>
2022-11-20 12:35:23 +0100

    prep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/elements/sidebar/sidebar.component';
+import { HeaderComponent } from './components/elements/header/header.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { BookComponent } from './components/pages/details/book/book.component';
+import { UserComponent } from './components/pages/details/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    RegisterComponent,
+    MainComponent,
+    BookComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
